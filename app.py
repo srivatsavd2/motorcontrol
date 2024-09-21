f2699350b9f305e6de376aa809241b5b93ae84a4
srivatsavd2 <136217897+srivatsavd2@users.noreply.github.com>
2024-09-21 11:30:56 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('control')
+def handle_control(data):
+    direction = data['direction']
+    print(f"Received command: {direction}")
+    socketio.emit('command_received', {'status': 'Command processed'})
+
+if __name__ == '__main__':
+    print("hi")
+    
+    socketio.run(app)
